d4fee2aa95723a1c6b9707444c0b238c578de507
rahulgrandhi13579 <88554453+rahulgrandhi13579@users.noreply.github.com>
2021-08-07 19:49:58 +0530

    Add files via upload

---
+++ src/Socialdistance_mask_detection.py
@@ -0,0 +1,231 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+import datetime
+from centroidtracker import CentroidTracker
+from itertools import combinations
+import math
+
+cap = cv2.VideoCapture("alley.mp4")
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    faces = []
+    locs = []
+    preds = []
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > args["confidence"]:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    if len(faces) > 0:
+
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+
+    return (locs, preds)
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", type=str,
+                default="face_detector",
+                help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+                default="mask_detector.model",
+                help="path to trained face mask detector model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+print("[INFO] loading face detector model...")
+weightsPath = os.path.sep.join([args["face"],"res10_300x300_ssd_iter_140000.caffemodel"])
+prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt"])
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+print("[INFO] loading face mask detector model...")
+maskNet = load_model(args["model"])
+
+print("[INFO] starting video stream...")
+time.sleep(2.0)
+protopath = "MobileNetSSD_deploy.prototxt"
+modelpath = "MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
+
+
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+           "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+           "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+           "sofa", "train", "tvmonitor"]
+
+tracker = CentroidTracker(maxDisappeared=5, maxDistance=50)
+
+
+def non_max_suppression_fast(boxes, overlapThresh):
+    try:
+        if len(boxes) == 0:
+            return []
+
+        if boxes.dtype.kind == "i":
+            boxes = boxes.astype("float")
+
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+
+        while len(idxs) > 0:
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            overlap = (w * h) / area[idxs[:last]]
+
+            idxs = np.delete(idxs, np.concatenate(([last],
+                                                   np.where(overlap > overlapThresh)[0])))
+
+        return boxes[pick].astype("int")
+    except Exception as e:
+        print("Exception occurred in non_max_suppression : {}".format(e))
+
+
+def main():
+
+    fps_start_time = datetime.datetime.now()
+    fps = 0
+    total_frames = 0
+
+    while True:
+
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=600)
+        (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+        for (box, pred) in zip(locs, preds):
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
+
+            label = "Mask" if mask > withoutMask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+            cv2.putText(frame, label, (startX, startY - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        total_frames = total_frames + 1
+
+        (H, W) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+
+        detector.setInput(blob)
+        person_detections = detector.forward()
+        rects = []
+        for i in np.arange(0, person_detections.shape[2]):
+            confidence = person_detections[0, 0, i, 2]
+            if confidence > 0.5:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] != "person":
+                    continue
+
+                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (startX, startY, endX, endY) = person_box.astype("int")
+                rects.append(person_box)
+
+        boundingboxes = np.array(rects)
+        boundingboxes = boundingboxes.astype(int)
+        rects = non_max_suppression_fast(boundingboxes, 0.3)
+        centroid_dict = dict()
+        objects = tracker.update(rects)
+        for (objectId, bbox) in objects.items():
+            x1, y1, x2, y2 = bbox
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            cX = int((x1 + x2) / 2.0)
+            cY = int((y1 + y2) / 2.0)
+
+
+            centroid_dict[objectId] = (cX, cY, x1, y1, x2, y2)
+
+        red_zone_list = []
+        for (id1, p1), (id2, p2) in combinations(centroid_dict.items(), 2):
+            dx, dy = p1[0] - p2[0], p1[1] - p2[1]
+            distance = math.sqrt(dx * dx + dy * dy)
+            if distance < 160.0:
+                if id1 not in red_zone_list:
+                    red_zone_list.append(id1)
+                if id2 not in red_zone_list:
+                    red_zone_list.append(id2)
+        ans=0
+        for id, box in centroid_dict.items():
+            print(id)
+            if id in red_zone_list:
+                ans=ans+1
+                cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 0, 255), 2)
+            else:
+                cv2.rectangle(frame, (box[2], box[3]), (box[4], box[5]), (0, 255, 0), 2)
+        cv2.putText(frame, "NO OF VIOLATIONS  " + str(ans), (10, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+        fps_end_time = datetime.datetime.now()
+        time_diff = fps_end_time - fps_start_time
+        if time_diff.seconds == 0:
+            fps = 0.0
+        else:
+            fps = (total_frames / time_diff.seconds)
+
+        fps_text = "FPS: {:.2f}".format(fps)
+
+        cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+        cv2.imshow("Application", frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+
+main()
